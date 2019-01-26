3011c1acfa5e032f0e40c4a2fa2b2fcac4dcef11
Fareed Murtaza <fareedmurtaza77@ucp.edu.pk>
2019-01-27 02:27:40 +0500

    Session and cookie added in Admin login page(Fareed)

--- final wad project/admin/index.php
+++ final wad project/admin/index.php
@@ -2,6 +2,8 @@
 require_once "../server/db_connection.php";
 $u = "";
 $p = "";
+$error_msg = '';
+session_start();
 if(isset($_POST['admin_login']))
 {
     $user = $_POST['admin_user'];
@@ -19,10 +21,23 @@ if(isset($_POST['admin_login']))
     $q = "select * from admins where username='$user' and password='$pass';";
     $qn = mysqli_query($con, $q);
     $check_user = mysqli_num_rows($qn);
-    if($check_user==0){
+    if($check_user==0)
+    {
         $error_msg = 'Password or Email is wrong, try again';
     }
-    else{
+    else
+    {
+        $_SESSION['admin_user'] = $user;
+        if(!empty($_POST['remember']))
+        {
+            setcookie('admin_user', $user, time() + (10 * 365 * 24 * 60 * 60));
+            setcookie('admin_password', $pass, time() + (10 * 365 * 24 * 60 * 60));
+        }
+        else
+        {
+            setcookie('admin_user','' );
+            setcookie('admin_password', '');
+        }
         header('location: menu.php');
     }
 }
@@ -46,13 +61,17 @@ if(isset($_POST['admin_login']))
 <div class="container">
     <h1 class="text-center my-4"> <span class="d-none d-sm-inline"> Admin </span> Login </h1>
     <form action="index.php" method="post" enctype="multipart/form-data">
+        <div class="text-center"><?php echo $error_msg;?></div>
+        <br>
         <div class="row">
             <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-4 mt-auto">
                 <label for="admin_username" class="float-md-right"> <span class="d-sm-none d-md-inline"> Username: </span></label>
             </div>
             <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
                 <div class="input-group">
-                    <input type="text" class="form-control" id="pro_title" name="admin_user" placeholder="Admin Username">
+                    <input
+                            value="<?php echo @$_COOKIE['admin_user']?>" type="text"
+                            class="form-control" id="pro_title" name="admin_user" placeholder="Admin Username">
                 </div>
             </div>
             <span><?php echo $u; ?></span>
@@ -64,10 +83,20 @@ if(isset($_POST['admin_login']))
             </div>
             <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
                 <div class="input-group">
-                    <input class="form-control" id="pro_price" name="admin_password" placeholder="Admin Password">
+                    <input
+                            value="<?php echo @$_COOKIE['admin_password']?>" type="password" class="form-control"
+                            id="pro_price" name="admin_password" placeholder="Admin Password">
                 </div>
             </div>
             <span><?php echo $p; ?></span>
+        </div>
+
+        <div class="row my-3">
+            <div class="col-sm-3 col-md-4 col-lg-2 col-xl-5 mt-auto"></div>
+            <div class="form-check align-center" style="padding-left: 50px">
+                <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                <label class="form-check-label" for="remember">Remember me</label>
+            </div>
         </div>
 
         <div class="row my-3">
@@ -78,6 +107,8 @@ if(isset($_POST['admin_login']))
         </div>
     </form>
 </div>
+<script src="../js/jquery-3.3.1.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
 </body>
 </html>
 
